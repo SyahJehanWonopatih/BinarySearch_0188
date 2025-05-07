eee508f096181999cf0e9107e428dcc75fde6f99
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-07 15:16:20 +0700

    menambahkan opsi pencarian ulang pada binary search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -98,5 +98,7 @@ void binarySearch()
         cout << "\nElemen " << x << " tidak ditemukan dalam array.\n";
     }
 
-   
+    cout << "\nIngin mencari lagi? (y/n): ";
+    cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
 }
