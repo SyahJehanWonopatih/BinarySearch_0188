6edd0f35fea5853544a5e65db3ea5ec2ff36e343
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-07 14:46:17 +0700

    menambahkan validasi agar input nPanjang tidak lebih dari 10

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -6,7 +6,17 @@ int nPanjang;
 int x;
 
 void input() {
-   
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array (1-10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10) {
+            break;
+        }
+        else {
+            cout << "\n[!] Jumlah elemen tidah boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+
     cout << "\n=========================================\n";
     cout << "            Masukkan Elemen Array        \n";
     cout << "=========================================\n";
