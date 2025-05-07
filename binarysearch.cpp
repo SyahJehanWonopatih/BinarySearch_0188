d040349f8c8024710aa5b4f2f2c82d9d5763b318
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-07 14:44:50 +0700

    membuat fungsi untuk input data array

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input() {
+   
+    cout << "\n=========================================\n";
+    cout << "            Masukkan Elemen Array        \n";
+    cout << "=========================================\n";
+    for (int i = 0; i < nPanjang; i++) {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
